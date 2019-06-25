c71eb4dccaf36506c122b89071dabd4fa5bc9415
natiacostap <nati_aap@hotmail.com>
2019-06-25 15:26:45 -0400

    lee rutas y las convierte en absolutas, lee directorios con filehound y lo muestra en consola

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const FileHound = require('filehound');
+
 let route = process.argv[2];
 route = path.resolve(route);
 console.log("Resolve:", route);
@@ -7,23 +9,41 @@ route = path.normalize(route);
 console.log("Normalize:", route);
 
 
-// const ReadDirWithFileHound = () => {
-//   const FileHound = require('filehound');
+  const getFilesFromFilehound = (path) => {
+  const files = FileHound.create()
+  .paths(route)
+  .ext('md')
+  .find();
 
-// const files = FileHound.create()
-//   .paths('route')
-//   .ext('md')
-//   .find();
-
-// files.then(console.log);
-// }
-
-// fs.readFile('README.md','utf-8',(error, data) => {
-// if(error){
-//   console.log(`Error ${error}`);
-// }else{
-//   console.log(data);
-// }
+  files
+  // .then(console.log)
+  .then(res => { console.log('response:', res);
+  res.forEach(element => {
+    getLinksFromFile();
+  })}
+  )};
 
 
-// })
+  const getLinksFromFile  = (path) => {
+  fs.readFile(route ,'utf-8',(error, data) => {
+  if(error)
+  console.log(`Error ${error}`);
+
+  let getLinks = [];
+   
+  const renderer = new marked.Renderer();
+  renderer.link = function(href, title, text){
+    getLinks.push({
+      // link que encuentra
+      href:href,
+      //texto que aparece dentro
+      text:text,
+      //ruta archivo
+      file:path
+    });
+  console.log(getLinks);
+  }
+  marked(data, {renderer:renderer})
+    console.log(getLinks)
+})
+}
